20f914c770ff673bc7fa34c551883f4c231181d7
Luke Bergen <lukeabergen@gmail.com>
2014-10-25 20:55:00 -0400

    Some more missed stuff

--- new_box.sh
+++ new_box.sh
@@ -4,18 +4,27 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install caskroom/cask/brew-cask
 
 brew install ctags
+
+brew cask install java
 brew install elasticsearch
+ln -sfv /usr/local/opt/elasticsearch/*.plist ~/Library/LaunchAgents
+
 brew install git
 brew install gnupg
 brew install go
+brew install heroku-toolbelt
 brew install imagemagick
 brew install keybase
 brew install mercurial
 brew install node
 brew install nvm
+
 brew install postgresql
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+
 brew install rbenv
 brew install redis
+brew install ruby-build
 brew install the_silver_searcher
 brew install vim
 brew install wget
@@ -35,6 +44,9 @@ brew cask install onepassword
 brew cask install skitch
 brew cask install skype
 brew cask install whatsapp-pocket
+
+gem install bundler
+gem install jekyll
 
 # install vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.dotfiles/vim/bundle/Vundle.vim
