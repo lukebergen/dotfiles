84c9900e924110ccb41fb042f57129f53f8db57a
Luke Bergen <lukeabergen@gmail.com>
2017-07-28 17:45:30 -0400

    More colors/completion stuff

--- zsh/themes/luke-bergen.zsh
+++ zsh/themes/luke-bergen.zsh
@@ -1,10 +1,17 @@
-function git_bit() {
-  local blue="\033[38;5;4m"
-  local red="\033[38;5;1m"
-  local yellow="\033[38;5;11m"
-  local green="\033[38;5;2m"
-  local reset_color="\033[38;5;15m"
+# Colors
+typeset -A colors
+colors=(
+  white "\033[38;5;15m"
+  blue "\033[38;5;4m"
+  red "\033[38;5;1m"
+  yellow "\033[38;5;11m"
+  green "\033[38;5;2m"
+  reset "\033[38;5;15m" # Just a shortcut for "white"
+)
 
+
+# Git prompt
+function git_bit() {
   if [ -d .git ]; then
     STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
     BRANCH=$(command git rev-parse --abbrev-ref HEAD)
@@ -14,12 +21,12 @@ function git_bit() {
     fi
 
     if [[ -n $STATUS ]]; then
-      echo "$blue($red$BRANCH $yellow✗$blue) $green$ $reset_color"
+      echo "$colors[blue]($colors[red]$BRANCH $colors[yellow]✗$colors[blue]) $colors[green]$ $colors[reset]"
     else
-      echo "$blue($red$BRANCH $green√$blue) $green$ $reset_color"
+      echo "$colors[blue]($colors[red]$BRANCH $colors[green]√$colors[blue]) $colors[green]$ $colors[reset]"
     fi
   else
-    echo "$green$ $reset_color"
+    echo "$colors[green]$ $colors[reset]"
   fi
 
   echo $result
@@ -27,3 +34,19 @@ function git_bit() {
 
 setopt promptsubst
 PROMPT='%1~ $(git_bit)'
+
+# Colors and completion stuff
+
+LSCOLORS=Gxfxcxdxbxegedabagacad
+LSCOLORS=exfxcxdxbxegedabagacad
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+export COMPLETION_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+
+#zstyle ':completion:*:default' menu select
+zstyle ':completion:*:default' menu select
+zstyle ':completion:*:default' list-colors "${(@s.:.)COMPLETION_COLORS}"
+
+#zstyle ':completion:*' menu select list-colors "${(@s.:.)LSCOLORS}"
+autoload -Uz compinit
+compinit
