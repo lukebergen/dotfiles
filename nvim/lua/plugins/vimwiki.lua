6ef8ade3be715faa3b93d64f3f148e0559bc54df
Luke Bergen <lukeabergen@gmail.com>
2024-10-17 08:44:48 -0400

    Making wiki style vimwiki index links work the way they used to

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -24,6 +24,7 @@ return {
         vim.g.vimwiki_list = {wiki}
         vim.g.vimwiki_listsyms = ' .oO√'
         vim.g.vimwiki_table_auto_fmt = 0 -- Disable auto formatting tables (it's screwing up my maps)
+        vim.g.vimwiki_dir_link = 'index'
       end
 
       -- vim.g.vimwiki_dir_link = 'index'
