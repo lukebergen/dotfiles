cd1a9356fbfac9f98ddffe98f2d318131632d182
Luke Bergen <lukeabergen@gmail.com>
2024-11-20 22:49:32 -0500

    winblend?! Also keymap to resume previous telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,13 +2,22 @@ return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "node_modules" }
-      },
-    },
+    -- why does this not work? It's supposed to
+    --opts = {
+    --  defaults = {
+    --    file_ignore_patterns = { "node_modules" },
+    --    winblend = 20,
+    --  },
+    --},
 
     config = function()
+      require("telescope").setup({
+        defaults = {
+          winblend = 30, -- let's see if this just ends up annoying us
+          file_ignore_patterns = { "node_modules" },
+        },
+      })
+
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, desc = "[f]ind [f]ile"})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { noremap = true, desc = "[f]ind files [g]repping through their content"})
@@ -17,6 +26,7 @@ return {
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { noremap = true, desc = "[f]ind open [b]uffer"})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { noremap = true, desc = "[f]ind in [h]elp"})
       vim.keymap.set('n', '<leader>fi', builtin.highlights, { noremap = true, desc = "[f]ind in [h]elp"})
+      vim.keymap.set('n', '<leader>fr', builtin.resume, { noremap = true, desc = "[r]esume previous search"})
     end
   }
 }
