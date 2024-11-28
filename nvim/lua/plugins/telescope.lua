f2c44debf5d4c5cbf9c4580ff900b6132b7261d7
Luke Bergen <lukeabergen@gmail.com>
2024-11-28 00:27:51 -0500

    And attempt at getting telescope to do image preview

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -17,6 +17,31 @@ return {
         defaults = {
           winblend = 20, -- let's see if this just ends up annoying us
           file_ignore_patterns = { "node_modules" },
+          preview = {
+            mime_hook = function(filepath, bufnr, opts)
+              local is_image = function(filepath)
+                local image_extensions = {'png','jpg'}   -- Supported image formats
+                local split_path = vim.split(filepath:lower(), '.', {plain=true})
+                local extension = split_path[#split_path]
+                return vim.tbl_contains(image_extensions, extension)
+              end
+              if is_image(filepath) then
+                local term = vim.api.nvim_open_term(bufnr, {})
+                local function send_output(_, data, _ )
+                  for _, d in ipairs(data) do
+                    vim.api.nvim_chan_send(term, d..'\r\n')
+                  end
+                end
+                vim.fn.jobstart(
+                  {
+                    'chafa', filepath  -- Terminal image viewer command
+                  },
+                  {on_stdout=send_output, stdout_buffered=true, pty=true})
+              else
+                require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
+              end
+            end
+          },
         },
       })
 
