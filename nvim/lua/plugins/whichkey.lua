eb0eec9bdbdedeb4af7357e4063c9c62b4425db2
Luke Bergen <lukeabergen@gmail.com>
2024-11-30 19:39:42 -0500

    Fixing `V` getting borked by which-key

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -3,18 +3,12 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      delay = 600
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
+      delay = 600,
+      -- I tried letting it just take over everything. But annoyingly, `V` started
+      -- acting weird. So now we'll just explicitly opt-in for most mappings
+      triggers = {
+        { "<leader>", mode = { "n", "v" } },
+        { "g", mode = { "n", "v" } },
       },
     },
   }
