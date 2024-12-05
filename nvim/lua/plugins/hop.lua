1576bbc88807e6882f95601db678dca445bd3025
Luke Bergen <lukeabergen@gmail.com>
2024-12-05 15:35:01 -0500

    pulling hop config into the hop plugin. todo: do this for everything

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -2,8 +2,31 @@ return {
   {
     'smoka7/hop.nvim',
     version = "*",
-    opts = {
+    config = function()
+      local hop = require('hop')
+      hop.setup({
         keys = 'etovxqpdygfblzhckisuran'
-    }
+      })
+      --local directions = require('hop.hint').HintDirection
+      vim.keymap.set('n', 's', function()
+        hop.hint_char1({ current_line_only = false })
+      end, {remap=true})
+
+      --[[
+      vim.keymap.set('n', 'f', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end, {remap=true})
+      vim.keymap.set('n', 'F', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end, {remap=true})
+      vim.keymap.set({'n', 'v'}, 't', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end, {remap=true})
+      vim.keymap.set({'n', 'v'}, 'T', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end, {remap=true})
+      --]]
+
+    end
   }
 }
