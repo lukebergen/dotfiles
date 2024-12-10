4d93552bb465b7242c1e27e2b948d747bcd886d1
Luke Bergen <lukeabergen@gmail.com>
2024-12-09 23:44:47 -0500

    checkpoint of _something_ working  with ollama

--- nvim/lua/luke/llm.lua
+++ nvim/lua/luke/llm.lua
@@ -1,24 +1,16 @@
+-- TODO: try doing this in a more structured way. Maybe chat with messages being more
+-- explicit about what's from the user and what's from the assistant?
 local json = require("lib.lunajson")
 
--- TODO: rethink about all of this given how we tend to use copilot
-
-
-local query = function(prompt, opts) --newWindow, skipBackticks)
+local system = "You are a helpful code assistant. Ignore any lines that looks like '--user--------' or '--system-----'. Do not include lines like '--system-----' in your outputs."
+local function doQuery(buffer)
   --local curlArgs = string.format([[curl -s -N -X POST http://localhost:11434/api/generate -d '{ "model": "codellama", "prompt": "%s" }']], prompt)
 
-  local newWindow = opts.newWindow ~= false
-  local skipBackticks = opts.skipBackticks == true
-
-  local outputBuffer = vim.api.nvim_get_current_buf()
-
-  if newWindow then
-    local startingWindow = vim.api.nvim_get_current_win()
-    vim.cmd("vnew")
-    outputBuffer = vim.api.nvim_get_current_buf()
-    vim.api.nvim_set_current_win(startingWindow)
-  end
-
-  prompt = string.gsub(prompt, "\n", "\\n"):gsub('"', "\\\"")
+  local lineCount = vim.api.nvim_buf_line_count(buffer)
+  local lines = vim.api.nvim_buf_get_lines(buffer, 0, lineCount, false)
+  local prompt = ""
+  for _, line in ipairs(lines) do prompt = prompt .. line .. "\n" end
+  prompt = prompt:gsub("\n", "\\n"):gsub('"', '\\"')
   local curlArgs = {
     "-s",
     "-N",
@@ -26,114 +18,77 @@ local query = function(prompt, opts) --newWindow, skipBackticks)
     "POST",
     "http://localhost:11434/api/generate",
     "-d",
-    string.format('{ "model": "codellama", "prompt": "%s" }', prompt)
+    string.format('{ "model": "codellama", "system": "%s", "prompt": "%s" }', system, prompt)
   }
+
+  -- place to debug. Print the curlArgs
 
   local stdin = vim.loop.new_pipe(false)
   local stdout = vim.loop.new_pipe(false)
   local stderr = vim.loop.new_pipe(false)
 
-  local handle, pid = vim.uv.spawn("curl", {
+  local handle
+  handle, _ = vim.uv.spawn("curl", {
     args = curlArgs,
     stdio = {stdin, stdout, stderr}
   }, function(code, signal)
-    -- on exit
+    stdout:read_stop()
+    stderr:read_stop()
+    stdout:close()
+    stderr:close()
+    if handle then handle:close() end
+
+    vim.schedule(function()
+      vim.api.nvim_buf_set_lines(buffer, -1, -1, false, {
+        "",
+        "--user-------",
+        "",
+      })
+    end)
+    -- we're done getting things back
   end)
+
+  vim.api.nvim_buf_set_lines(buffer, -1, -1, false, {
+    "",
+    "--system-----",
+    "",
+  })
 
   vim.uv.read_start(stdout, function(_, data)
     if data then
       vim.schedule(function()
         local object = json.decode(data)
-        local cleaned = object.response
-        if cleaned and (not skipBackticks or not cleaned:find("^```")) then
-          local lastLine = vim.api.nvim_buf_get_lines(outputBuffer, -2, -1, false)[1] or ""
+        if object then
+          local cleaned = object.response
+          local lastLine = vim.api.nvim_buf_get_lines(buffer, -2, -1, false)[1] or ""
           local newLastLine = vim.split(lastLine .. cleaned, "\n")
-          vim.api.nvim_buf_set_lines(outputBuffer, -2, -1, false, newLastLine)
+          vim.api.nvim_buf_set_lines(buffer, -2, -1, false, newLastLine)
         end
       end)
     end
   end)
 end
 
--- maybe in future we can pull from other things like the full buffer or even other open buffers?
--- todo: make this less goofy. Look for examples and such
-local promptFrom = function(input, context)
-  return string.format([[
-You are a helpful and skilled programmer who answers questions simply and concisely.
-Here is some context:
+-- do a bunch of shenanigans to turn this window into an LLMy chat window kinda thing
+-- for now, not going to worry about undoing it. Once done, just blow the buffer away
+vim.api.nvim_create_user_command("LLM", function()
+  vim.b.IS_LLM = true
+  vim.bo.filetype = "markdown"
+  local buffer = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_set_lines(buffer, 0, 0, false, {
+    "system: yes?",
+    "",
+    "--user--------",
+    "",
+  })
 
-%s
-
----
-
-Given this context, please answer the following question:
-
-%s
-  ]], context, input)
-end
-
-local codeOnlyPrompt = function(input, context)
-  return string.format([[
-You are a helpful and skilled programmer. You only respond with code. You do not include backticks to demarcate chunks of code. The code you provide is syntactically correct and complete on its own unless it refers to something in context. Do not include backticks.
-Here is some context:
-
----
-%s
----
-
-Given this context, write code for the following: %s]], context, input)
-end
-
-local getContext = function(opts)
-  local startLine = opts.line1
-  local endLine = opts.line2
-  local rangeContent = ""
-
-  if opts.range == 0 then
-    -- no range selected
-  elseif opts.range == 1 then
-    -- single line range
-    local lines = vim.api.nvim_buf_get_lines(0, startLine - 1, endLine, false)
-    rangeContent = table.concat(lines, "\n")
-  else
-    -- multi-line range
-    local lines = vim.api.nvim_buf_get_lines(0, startLine - 1, endLine, false)
-    local lines = vim.api.nvim_buf_get_lines(0, startLine - 1, endLine, false)
-    rangeContent = table.concat(lines, "\n")
-  end
-
-  return rangeContent
-end
-
-vim.api.nvim_create_user_command("LLM", function(opts)
-  local input = opts.args
-  local context = getContext(opts)
-  local prompt = promptFrom(input, context)
-  query(prompt, {newWindow = true})
+  vim.api.nvim_buf_set_keymap(0, 'n', '<enter>', '', {
+    noremap = true,
+    silent = true,
+    callback = function()
+      doQuery(buffer)
+    end
+  })
 end, {
-  nargs = '?',
-  range = true,
-  desc = 'Prompt an LLM'
+  desc = 'Set current buffer up as an LLM assistant window'
 })
-
-vim.api.nvim_create_user_command("C", function(opts)
-  local input = opts.args
-  local context = getContext(opts)
-  local prompt = codeOnlyPrompt(input, context)
-  query(prompt, {newWindow = true, skipBackticks = true})
-end, {
-  nargs = '?',
-  range = true,
-  desc = 'Prompt an LLM'
-})
-
---vim.api.nvim_create_user_command("C", function(opts)
---  print("still working on that one")
---  --local input = opts.args
---  --local context = ""
---  --local prompt = promptFrom(input, context)
---  --query(prompt, false)
---end, {
---  nargs = '?',
---  desc = 'Write some code in the current buffer'
---})
