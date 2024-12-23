1e03132fd1ef8a00421b52ee54aadc98d4f830e1
Luke Bergen <lukeabergen@gmail.com>
2024-12-22 22:15:52 -0500

    LLM: handling messages more cleanly

--- nvim/lua/luke/llm.lua
+++ nvim/lua/luke/llm.lua
@@ -1,30 +1,49 @@
--- TODO: try doing this in a more structured way. Maybe chat with messages being more
--- explicit about what's from the user and what's from the assistant?
 local json = require("lib.lunajson")
 
 local model = "codellama:latest" -- smaller, faster
 --local model = "codellama:34b" -- bigger, maybe more accurate? seems slower
 
-local system = "You are a helpful code assistant. Ignore any lines that looks like '# User---' or '# System---'. Do not include lines like '# System---' in your outputs."
-local function doQuery(buffer)
-  --local curlArgs = string.format([[curl -s -N -X POST http://localhost:11434/api/generate -d '{ "model": "codellama", "prompt": "%s" }']], prompt)
+local state = {messages = {}, nextResponse = ""}
 
+-- definitely a work in progress, this one.
+-- TODO: different system prompts for different situations
+local system = "You are a helpful code assistant. You answer in concise but polite messages. When you give code examples you wrap them in markdown code blocks for the appropriate language."
+-- e.g. pirate mode? lol
+--local system = "You are a helpful code assistant. You answer in concise but polite messages. When you give code examples you wrap them in markdown code blocks for the appropriate language."
+
+-- can do as buf local commands (see Debug and Reset exampels at bottom)
+
+local function reset()
+  for i = 1, #state.messages do
+    state.messages[i] = nil
+  end
+  table.insert(state.messages, {role = "system", content = system})
+end
+
+local function doQuery(buffer)
   local lineCount = vim.api.nvim_buf_line_count(buffer)
   local lines = vim.api.nvim_buf_get_lines(buffer, 0, lineCount, false)
   local prompt = ""
-  for _, line in ipairs(lines) do prompt = prompt .. line .. "\n" end
+
+  for _, line in ipairs(lines) do
+    if line == "# User---" or line == "# System---" then
+      prompt = ""
+    else
+      prompt = prompt .. line .. "\n"
+    end
+  end
+
   prompt = prompt:gsub("\n", "\\n"):gsub('"', '\\"')
+  table.insert(state.messages, {role = "user", content = prompt})
   local curlArgs = {
     "-s",
     "-N",
     "-X",
     "POST",
-    "http://localhost:11434/api/generate",
+    "http://localhost:11434/api/chat",
     "-d",
-    string.format('{ "model": "%s", "system": "%s", "prompt": "%s" }', model, system, prompt)
+    string.format('{"model": "%s", "messages": %s}', model, json.encode(state.messages))
   }
-
-  -- place to debug. Print the curlArgs
 
   local stdin = vim.loop.new_pipe(false)
   local stdout = vim.loop.new_pipe(false)
@@ -34,7 +53,7 @@ local function doQuery(buffer)
   handle, _ = vim.uv.spawn("curl", {
     args = curlArgs,
     stdio = {stdin, stdout, stderr}
-  }, function(code, signal)
+  }, function(_, _)
     stdout:read_stop()
     stderr:read_stop()
     stdout:close()
@@ -50,7 +69,8 @@ local function doQuery(buffer)
       })
       vim.api.nvim_feedkeys("G$", "n", true)
     end)
-    -- we're done getting things back
+    table.insert(state.messages, {role = "assistant", content = state.nextResponse})
+    state.nextResponse = ""
   end)
 
   vim.api.nvim_buf_set_lines(buffer, -1, -1, false, {
@@ -65,10 +85,13 @@ local function doQuery(buffer)
       vim.schedule(function()
         local object = json.decode(data)
         if object then
-          local cleaned = object.response
-          local lastLine = vim.api.nvim_buf_get_lines(buffer, -2, -1, false)[1] or ""
-          local newLastLine = vim.split(lastLine .. cleaned, "\n")
-          vim.api.nvim_buf_set_lines(buffer, -2, -1, false, newLastLine)
+          if not object.done then
+            local cleaned = object.message.content
+            state.nextResponse = state.nextResponse .. cleaned
+            local lastLine = vim.api.nvim_buf_get_lines(buffer, -2, -1, false)[1] or ""
+            local newLastLine = vim.split(lastLine .. cleaned, "\n")
+            vim.api.nvim_buf_set_lines(buffer, -2, -1, false, newLastLine)
+          end
 
           vim.api.nvim_feedkeys("G$", "n", true)
         end
@@ -77,10 +100,10 @@ local function doQuery(buffer)
   end)
 end
 
+-- TODO: figure out why this doesn't seem to actually work. Annoyingly, it's
+-- hard to debug because the first time we check to see if we get a fast
+-- response, that check warms it up
 local function warmItUp()
---curl http://localhost:11434/api/generate -d '{
---  "model": "llama3.2"
---}'
   local curlArgs = {
     "-s",
     "-N",
@@ -100,6 +123,7 @@ vim.api.nvim_create_user_command("LLM", function()
   vim.bo.filetype = "markdown"
   vim.bo.buftype = "nofile"
   warmItUp()
+  reset()
   local buffer = vim.api.nvim_get_current_buf()
   vim.api.nvim_buf_set_lines(buffer, 0, 0, false, {
     "# System---",
@@ -116,6 +140,30 @@ vim.api.nvim_create_user_command("LLM", function()
     callback = function()
       doQuery(buffer)
     end
+  })
+
+  vim.api.nvim_buf_create_user_command(0, "Reset", function()
+    reset()
+    vim.api.nvim_feedkeys("ggVGd", "n", true)
+    vim.schedule(function()
+      vim.api.nvim_buf_set_lines(buffer, 0, 0, false, {
+        "# System---",
+        "",
+        "yes?",
+        "",
+        "# User---",
+        "",
+      })
+    end)
+  end, {
+    desc = 'Reset the conversation'
+  })
+  vim.api.nvim_buf_create_user_command(0, "Debug", function()
+    local str = json.encode(state)
+    local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+    vim.api.nvim_buf_set_lines(0, line, line, false, {str})
+  end, {
+    desc = 'Debug things'
   })
 end, {
   desc = 'Set current buffer up as an LLM assistant window'
