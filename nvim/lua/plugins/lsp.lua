c2d4694b6b98aca01a5995838b9ca01c64b238f0
Luke Bergen <lukeabergen@gmail.com>
2025-01-08 12:01:16 -0500

    working on completion stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -312,6 +312,7 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-buffer',
     },
     config = function()
       -- See `:help cmp`
@@ -327,7 +328,9 @@ return {
         },
         completion = {
           -- let's see how we feel about this
-          -- autocomplete = false,
+          -- will automatically show completion options. false requires a keymap to trigger your options
+          --autocomplate=true,
+          autocomplete = false,
           completeopt = 'menu,menuone,noinsert',
         },
 
@@ -335,55 +338,30 @@ return {
         -- chosen, you will need to read `:help ins-completion`
         --
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
+
+        --[[ my notes/thoughts:
+          auto-suggest: mixed feelings. Will jump back and forth to decide if I like it or not. But...
+          we do want consistent mappings between each option so that muscle memory doesn't get a vote. Soo...
+          c-n / c-p: yes. Obvious. next/previous suggestion
+          c-l: accept completion
+          ~c-y: open suggestions (when auto-suggest is off)~
+
+          This means giving up c-l and c-h for navigating a snippet's expansion. But honestly, I
+          like vim for its motions. Do we really need mappings to jump around within a snippet template?
+          ... or not even. c-n / c-p will open completion if not open already which frees up c-y
+          though I still kind of like c-l for accepting completion instead of 'y' and I don't hate
+          giving up snippet navigation anyway...
+          only problem: cannot for the life of me get the same behavior in command line mode
+          C-z brings it up for whatever reason... good enough for now till we can dig in further?
+        ]]--
+        mapping = {
           ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          --['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          --['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+          ['<C-l>'] = cmp.mapping.confirm { select = true },
+          -- default: in command-line mode, for some reason C-z opens suggestions...
         },
         sources = {
+          { name = "buffer" },
           {
             name = 'lazydev',
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
