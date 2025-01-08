0859f49e0e901a4db5abbc09f37688a57db4c59c
Luke Bergen <lukeabergen@gmail.com>
2025-01-08 14:25:35 -0500

    fixing completion. right now it's c-z though so more tweaking coming

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -29,36 +29,29 @@ return {
         completion = {
           -- let's see how we feel about this
           -- will automatically show completion options. false requires a keymap to trigger your options
-          --autocomplate=true,
-          autocomplete = false,
+          -- note: autocomplete needs to be either false or a table
+          autocomplete=false,
           completeopt = 'menu,menuone,noinsert',
         },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-
-        --[[ my notes/thoughts:
-          auto-suggest: mixed feelings. Will jump back and forth to decide if I like it or not. But...
-          we do want consistent mappings between each option so that muscle memory doesn't get a vote. Soo...
-          c-n / c-p: yes. Obvious. next/previous suggestion
-          c-l: accept completion
-          ~c-y: open suggestions (when auto-suggest is off)~
-
-          This means giving up c-l and c-h for navigating a snippet's expansion. But honestly, I
-          like vim for its motions. Do we really need mappings to jump around within a snippet template?
-          ... or not even. c-n / c-p will open completion if not open already which frees up c-y
-          though I still kind of like c-l for accepting completion instead of 'y' and I don't hate
-          giving up snippet navigation anyway...
-          only problem: cannot for the life of me get the same behavior in command line mode
-          C-z brings it up for whatever reason... good enough for now till we can dig in further?
-        ]]--
-        mapping = {
+        mapping = cmp.mapping.preset.insert {
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-l>'] = cmp.mapping.confirm { select = true },
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-z>'] = cmp.mapping.complete(),
           -- default: in command-line mode, for some reason C-z opens suggestions...
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
         },
         sources = {
           { name = "buffer" },
