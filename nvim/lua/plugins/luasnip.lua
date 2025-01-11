8312982ef44a4f5adfec144fbb28a2f9249b0fee
Luke Bergen <lukeabergen@gmail.com>
2025-01-11 00:19:30 -0500

    telling luasnip to treat ejs files as html

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -18,6 +18,7 @@ return {
         'rafamadriz/friendly-snippets',
         config = function()
           require('luasnip.loaders.from_vscode').lazy_load()
+          require("luasnip").filetype_extend("jst", {"html"} )
         end,
       },
     },
