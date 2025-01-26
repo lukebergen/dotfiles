807019cfd51ea16d7ef7c0060ea27f3b947ba519
Luke Bergen <lukeabergen@gmail.com>
2025-01-26 18:31:21 -0500

    making which-key show spelling suggestions immediately

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -3,7 +3,13 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      delay = 600,
+      delay = function(ctx)
+        if (ctx.keys == 'z=') then
+          return 0
+        else
+          return 600
+        end
+      end,
       -- I tried letting it just take over everything. But annoyingly, `V` started
       -- acting weird. So now we'll just explicitly opt-in for most mappings
       triggers = {
