ade48668ba72aff4adcf1666c4d607d71ab2b690
Luke Bergen <lukeabergen@gmail.com>
2026-01-20 10:35:24 -0500

    some vimwiki changes now that we're on markdown TODO: all md now so...

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -10,8 +10,8 @@ return {
       }
 
       local wikiMd = {
-        path = '~/.vimwiki-md',
-        path_html = '~/.vimwiki-md/build',
+        path = '~/.vimwiki',
+        path_html = '~/.vimwiki/build',
         syntax = 'markdown',
         ext = '.md',
         auto_tags = 1,
@@ -20,21 +20,21 @@ return {
       if (os.getenv("WIKI_FORMAT") == "md") then
         vim.g.vimwiki_list = {wikiMd}
         vim.g.vimwiki_markdown_link_ext = 1
+        vim.g.vimwiki_dir_link = 'index'
+        vim.g.vimwiki_table_auto_fmt = 0
       else
         vim.g.vimwiki_list = {wiki}
         vim.g.vimwiki_listsyms = ' .oO√'
         vim.g.vimwiki_table_auto_fmt = 0 -- Disable auto formatting tables (it's screwing up my maps)
         vim.g.vimwiki_dir_link = 'index'
       end
-
-      -- vim.g.vimwiki_dir_link = 'index'
     end,
     config = function()
 
       -- to keymap:
       vim.keymap.set('n', '<leader>ww', function()
         if (os.getenv("WIKI_FORMAT") == "md") then
-          vim.cmd('lcd ~/.vimwiki-md')
+          vim.cmd('lcd ~/.vimwiki')
         else
           vim.cmd('lcd ~/.vimwiki')
         end
@@ -50,9 +50,7 @@ return {
         local today = os.date("!%Y-%m-%d")
         -- TODO: this could be cleaner. Also, figure out, from usage, what a good starting template might look like
         if (os.getenv("WIKI_FORMAT") == "md") then
-          table.insert(template, string.format("# %s", today))
-          table.insert(template, "")
-          table.insert(template, ":private-1:")
+          table.insert(template, string.format("[/](/) > [diary](/diary/) > %s", today))
           table.insert(template, "")
           table.insert(template, "## Todo")
           table.insert(template, "")
